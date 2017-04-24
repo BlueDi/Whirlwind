019adc2941f37a407e7b724af08d5627be1acf19
Diogo Cruz <diogohmcruz@hotmail.com>
2017-04-24 14:36:56 +0100

    Format

--- Whirlwind/src/logic/Heur.java
+++ Whirlwind/src/logic/Heur.java
@@ -2,27 +2,31 @@ package logic;
 
 /**
  * Classe para juntar vários elementos que classificam uma jogada.
+ * 
  * @author diogo
  *
  */
 class Heur {
 	public int row;
-	public int col; 
+	public int col;
 	public int movement;
 	public int value;
 
-	public void moveCalculator(){
-		switch (movement){
-		case 0:	//up
+	/**
+	 * Calcula onde a peça irá ficar.
+	 */
+	public void moveCalculator() {
+		switch (movement) {
+		case 0: // up
 			row--;
 			break;
-		case 1:	//right
+		case 1: // right
 			col++;
 			break;
-		case 2:	//down
+		case 2: // down
 			row++;
 			break;
-		case 3:	//left
+		case 3: // left
 			col--;
 			break;
 		default:

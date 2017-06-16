8d9fe6363d7206412d4060e71d6414f74ac92aac
Diogo Cruz <diogohmcruz@hotmail.com>
2017-06-16 22:01:49 +0100

    Tirar uma pasta desnecessária

--- test/logic/GameTest.java
+++ test/logic/GameTest.java
@@ -2,7 +2,7 @@ package logic;
 
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class GameTest {
     @Test
